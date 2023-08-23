817487e9d07d0b17b1b73e5684c75737fc181aae
madhi43 <103943383+madhi43@users.noreply.github.com>
2023-08-23 08:27:55 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,68 @@
 import java.util.Scanner;
 
 public class Main {
+    private boolean isNormalMode;
 
-    private String mode;
-
-    public Main(String mode) {
-        this.mode = mode;
+    public Main(boolean isNormalMode) {
+        this.isNormalMode = isNormalMode;
     }
 
-    public int add(int... numbers) {
-        if (mode.equals("Normal")) {
-            int sum = 0;
-            for (int num : numbers) {
-                sum += num;
-            }
-            return sum;
-        } else if (mode.equals("Opposite")) {
-            int result = numbers[0];
-            for (int i = 1; i < numbers.length; i++) {
-                result -= numbers[i];
-            }
-            return result;
+    public int add(int a, int b) {
+        if (isNormalMode) {
+            return a + b;
         } else {
-            throw new IllegalArgumentException("Invalid mode. Use 'Normal' or 'Opposite'.");
+            return a - b;
         }
     }
 
-    public int subtract(int... numbers) {
-        if (mode.equals("Normal")) {
-            int result = numbers[0];
-            for (int i = 1; i < numbers.length; i++) {
-                result -= numbers[i];
-            }
-            return result;
-        } else if (mode.equals("Opposite")) {
-            int sum = 0;
-            for (int num : numbers) {
-                sum += num;
-            }
-            return sum;
+    public int subtract(int a, int b) {
+        if (isNormalMode) {
+            return a - b;
         } else {
-            throw new IllegalArgumentException("Invalid mode. Use 'Normal' or 'Opposite'.");
+            return a + b;
         }
+    }
+
+    public int performOperation(int a, int b, String operation) {
+        if ("+".equalsIgnoreCase(operation)) {
+            return add(a, b);
+        } else if ("-".equalsIgnoreCase(operation)) {
+            return subtract(a, b);
+        } else {
+            throw new IllegalArgumentException("Invalid operation: " + operation);
+        }
+    }
+
+    public int performOperation(int a, int b, int c, String operation) {
+        int result = performOperation(a, b, operation);
+        return performOperation(result, c, operation);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter mode (Normal/Opposite): ");
-        String mode = scanner.nextLine();
-        Main calculator = new Main(mode);
+        System.out.print("Enter calculator mode (normal/opposite): ");
+        String modeInput = scanner.nextLine();
+        boolean isNormalMode = modeInput.equalsIgnoreCase("normal");
 
-        System.out.print("Enter numbers (separated by spaces): ");
-        String[] numStrings = scanner.nextLine().split(" ");
-        int[] numbers = new int[numStrings.length];
-        for (int i = 0; i < numStrings.length; i++) {
-            numbers[i] = Integer.parseInt(numStrings[i]);
+        Main calculator = new Main(isNormalMode);
+
+        System.out.print("Enter operation (+/-): ");
+        String operation = scanner.nextLine();
+
+        System.out.print("Enter numbers: \n");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int result;
+        if (scanner.hasNextInt()) {
+            int num3 = scanner.nextInt();
+            result = calculator.performOperation(num1, num2, num3, operation);
         }
-
-        int resultAdd = calculator.add(numbers);
-        int resultSubtract = calculator.subtract(numbers);
-
-        System.out.println("\n" + mode + " Mode:");
-        System.out.println("Addition: " + resultAdd);
-        System.out.println("Subtraction: " + resultSubtract);
+        else
+        {
+            result = calculator.performOperation(num1, num2, operation);
+        }
+        System.out.println("Result: " + result);
 
         scanner.close();
     }
